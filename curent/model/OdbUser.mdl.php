99e07cb33f7996bb38739cc2631d74f2f7a5ae85
benoit <benoitelie1@gmail.com>
2013-12-14 01:31:13 +0100

    passage en SQL du .mld User, et ajout method checkHash

--- curent/model/OdbUser.mdl.php
+++ curent/model/OdbUser.mdl.php
@@ -1,26 +1,54 @@
 <?php
-class OdbVelo{
+class OdbUser{
 
 	private $oNosql;
 
 	public function __construct()
 	{
-		$this->oNosql = $_SESSION['nosql'];
+		$this->oBdd = $_SESSION['bdd'];
 	}
 
-	public function estUser($matricule)
+	public function estUser($nom)
 	{
-		if(!empty($matricule))
-			return $this->oNosql->query('user', $matricule);
+		if(!empty($nom))
+		{
+			$req = 'SELECT COUNT(*) AS nb
+					FROM USER
+					WHERE Use_Nom = :nom';
+
+			$data = $this->oBdd->query($req , array('nom'=>$nom), Bdd::SINGLE_RES);
+
+			return (bool) $data->nb;
+		}
 
 		return false;
 	}
 
+	/**
+	 * verifie que le mot de passe est bon
+	 *
+	 * prend un hash du mot de passe,
+	 * le nom de l'utilisateur,
+	 * et cherche une entree qui valides les deux
+	 *
+	 * @param  string $nom  le nom du user
+	 * @param  string $hash le hash du mdp user
+	 * @return bool       valide ou non l'existance
+	 */
+	public function checkHashUser($nom, $hash)
+	{
+		if(!empty($nom) and !empty($hash))
+		{
+			$req = 'SELECT COUNT(*) AS nb
+					FROM USER
+					WHERE Use_Nom = :nom
+						AND Use_Hash = :hash';
 
-	public function getHashUser($matricule){
-		// retourne le mot de passe hashe (crypte)
-		$leHashUser = $this->oNosql->query('user', $matricule, Nosql::GET_RESULT);
+			$data = $this->oBdd->query($req , array('nom'=>$nom, 'hash'=>$hash), Bdd::SINGLE_RES);
 
-		return $leHashUser;
+			return (bool) $data->nb;
+		}
+
+		return false;
 	}
 }
