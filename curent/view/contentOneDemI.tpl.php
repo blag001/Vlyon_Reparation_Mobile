03576bcd8e49108583061581cb920cebe475cf05
benoit <benoitelie1@gmail.com>
2013-12-21 23:42:33 +0100

    ajout bouton 'Intervenir' sur le detail des bons Inter

--- curent/view/contentOneDemI.tpl.php
+++ curent/view/contentOneDemI.tpl.php
@@ -60,5 +60,14 @@
 			?>
 		</tbody>
 	</table>
+	<form class="form-add" role="form" action="index.php" method="GET" >
+		<input type="hidden" name="page" class="form-control" value="intervention" required >
+		<input type="hidden" name="action" class="form-control" value="creerbonintervention" required >
+		<input type="hidden" name="valeur" class="form-control" <?php
+			if(!empty($arg['uneDemandeInter']))
+				echo 'value="'.$arg['uneDemandeInter']->DemI_Num.'" ';
+			?> required >
+        <button type="submit" class="btn btn-lg btn-primary btn-block" >Intervenir</button>
+	</form>
 
 </div><!-- /.container -->
